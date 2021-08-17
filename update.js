6a2f5edb5a984e8afedb71508fde3dad30c4e8e0
SolChanP <qkrthfcks1@naver.com>
2021-08-18 06:52:10 +0900

    SecretKey 설정 및 401에러처리 추가

--- update.js
+++ update.js
@@ -2,15 +2,20 @@ const dotenv = require('dotenv');
 const axios = require('axios');
 
 dotenv.config();
+const header = { key: process.env.SECRETKEY }
+
 const update = async (type, data) => {
     const date = new Date();
     // /schedule
-    await axios.post(`${process.env.BASEURL}/${type}`, data)
+    await axios.post(`${process.env.BASEURL}/${type}`, data, { headers: header})
         .then(() => {
             console.log(`${type}/Updated - ${date}`); // 추후 로그 파일 남기는 것으로 업데이트 예정
         })
         .catch((error) => {
-        console.error("서버에서 문제가 발생하였습니다."); // 추후 로그 파일 남기는 것으로 업데이트 예정
+            if(error.response.status == 401)
+                console.error(`Error/401 - ${date}`);
+            else
+                console.error(`Error/Server - ${date}`); // 추후 로그 파일 남기는 것으로 업데이트 예정
     });
 }
 
