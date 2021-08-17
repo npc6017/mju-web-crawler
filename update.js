79d7a7edb9903b580ac904ff5285a976680f2eab
SolChanP <qkrthfcks1@naver.com>
2021-08-18 05:34:53 +0900

    서버 응답 예외 처리

--- update.js
+++ update.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 dotenv.config();
 const update = async (type, data) => {
     // /schedule
-    await axios.post(`${process.env.BASEURL}/${type}`, data);
+    await axios.post(`${process.env.BASEURL}/${type}`, data).catch((error) => {
+        console.error("서버에서 문제가 발생하였습니다.")
+    });
 }
 
 module.exports = update;
