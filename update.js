21c0268ed5d8c5d69fb091bc5326bbd8b485afae
SolChanP <qkrthfcks1@naver.com>
2021-08-18 05:47:13 +0900

    서버 요청 후처리 구현

--- update.js
+++ update.js
@@ -3,9 +3,14 @@ const axios = require('axios');
 
 dotenv.config();
 const update = async (type, data) => {
+    const date = new Date();
     // /schedule
-    await axios.post(`${process.env.BASEURL}/${type}`, data).catch((error) => {
-        console.error("서버에서 문제가 발생하였습니다.")
+    await axios.post(`${process.env.BASEURL}/${type}`, data)
+        .then(() => {
+            console.log(`${type}/Updated - ${date}`); // 추후 로그 파일 남기는 것으로 업데이트 예정
+        })
+        .catch((error) => {
+        console.error("서버에서 문제가 발생하였습니다."); // 추후 로그 파일 남기는 것으로 업데이트 예정
     });
 }
 
